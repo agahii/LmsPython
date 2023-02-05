1d4365301d5b5913fddda2cffc58a425dfbf5f65
Syed Ahmed Fraz Zaki <agahii@hotmail.com>
2023-02-05 17:58:44 +0500

    khjkhjk

--- school.py
+++ school.py
@@ -1,13 +1,8 @@
 class school:
+    List_grade=[]
     def __init__(self,name,address):
         self.name=name
         self.address=address
-
-
-
-
-
-
-
-
-
+    @classmethod    
+    def addgrade(cls,_grade):
+       cls.List_grade.append(_grade)
