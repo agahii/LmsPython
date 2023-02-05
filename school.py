f5d7c2b356331a6744bc735ea6948f1783bc4863
Syed Ahmed Fraz Zaki <agahii@hotmail.com>
2023-02-05 18:07:56 +0500

    aaaa

--- school.py
+++ school.py
@@ -1,12 +1,19 @@
 import student
 class school:
-   mystudent=[]
+   List_Students=[]
+   List_grades=[]
    def __init__(self,name,address):
         self.name=name
         self.address=address
 @classmethod
 def addstudent( cls,s1):
-        cls.mystudent.append(s1)
+        cls.List_Students.append(s1)
+@classmethod    
+def addgrade(cls,_grade):
+    cls.List_grades.append(_grade)
+
+
+
         
 
 
